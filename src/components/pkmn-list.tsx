7e5e1c09d7e98984833aee9703a0acfb24666af0
Anthony M <ampats2008@gmail.com>
2022-04-10 18:42:24 -0400

    updated filter with useMemo

--- src/components/pkmn-list.tsx
+++ src/components/pkmn-list.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, forwardRef, useRef } from "react"
+import React, { useState, useEffect, forwardRef, useRef, useMemo } from "react"
 import PkmnCard from "./pkmn-card"
 
 type Props = {
@@ -19,37 +19,40 @@ function PokemonList(
   { pkmn, itemCount, itemsPerPage, searchTerm }: Props,
   lastCardRef: React.Ref<HTMLDivElement> | undefined
 ) {
+  const filteredPkmn = useMemo(
+    () =>
+      Object.entries(pkmn).filter((pkmnObj) =>
+        pkmnObj[1].name.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    [pkmn, searchTerm]
+  )
+
   return (
     <>
       <div id="mainCardCont" className="cardCont">
-        {Object.entries(pkmn)
-          .filter((pkmnObj) =>
-            pkmnObj[1].name.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .slice(0, itemCount)
-          .map(([key, obj], i) => {
-            if (i === itemCount - 1) {
-              // if this is the last card, attach the intersection observer ref for infinite scrolling
-              return (
-                <PkmnCard
-                  ref={lastCardRef}
-                  key={`${key}__${obj.name}`}
-                  obj={obj}
-                  animationOrder={i}
-                  itemsPerPage={itemsPerPage}
-                />
-              )
-            } else {
-              return (
-                <PkmnCard
-                  key={`${key}__${obj.name}`}
-                  obj={obj}
-                  animationOrder={i}
-                  itemsPerPage={itemsPerPage}
-                />
-              )
-            }
-          })}
+        {filteredPkmn.slice(0, itemCount).map(([key, obj], i) => {
+          if (i === itemCount - 1) {
+            // if this is the last card, attach the intersection observer ref for infinite scrolling
+            return (
+              <PkmnCard
+                ref={lastCardRef}
+                key={`${key}__${obj.name}`}
+                obj={obj}
+                animationOrder={i}
+                itemsPerPage={itemsPerPage}
+              />
+            )
+          } else {
+            return (
+              <PkmnCard
+                key={`${key}__${obj.name}`}
+                obj={obj}
+                animationOrder={i}
+                itemsPerPage={itemsPerPage}
+              />
+            )
+          }
+        })}
       </div>
     </>
   )
